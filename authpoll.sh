94da2df9929fc9cfeefc5424a1c2282cda93f413
JerryHopper <hopper.jerry@gmail.com>
2020-11-25 16:30:16 +0100

    Create authpoll.sh

---
+++ authpoll.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+CLIENT_ID="FUSIONAUTH_CLIENT_ID"
+TOKEN_URL="https://FUSIONAUTH/oauth2/token"
+
+EXPIRES=1800
+INTERVAL=5
+
+end=$((SECONDS+EXPIRES))
+
+while [  $SECONDS -lt  $end ] ;  do
+		sleep $INTERVAL
+		AJSON=$(curl -s -X POST -F "client_id=${CLIENT_ID}" -F "device_code=${DEVICE_CODE}" -F "grant_type=urn:ietf:params:oauth:grant-type:device_code" ${TOKEN_URL})
+    HAS_ERROR="$(echo $AJSON|jq -r .error)"
+    if [ -z "$HAS_ERROR" ]; then
+          end=$((SECONDS))
+    else
+          if [ "$HAS_ERROR" != "authorization_pending" ]; then
+            echo "$AJSON"
+            exit 1
+          fi
+    fi
+done
+
+echo "$AJSON"
+exit 0
