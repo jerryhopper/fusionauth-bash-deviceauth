393269f8a5b7e7ab43d848713c9075fa22067e47
jerryhopper <hopper.jerry@gmail.com>
2020-12-03 21:34:46 +0100

    update...

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,26 +2,28 @@
 
 echo "FUSIONAUTH_API_KEY=$FUSIONAUTH_API_KEY"
 echo "APPLICATION_ID=$APPLICATION_ID"
+echo "DISCOVERY_URL=$DISCOVERY_URL"
 
 
 
-OPENID_CONFIG="$(curl -s "http://localhost:9011/.well-known/openid-configuration")"
+OPENID_CONFIG="$(curl -s "$DISCOVERY_URL")"
 
 
 #curl -s -o "$HOME/application.json" -H 'Authorization: $FUSIONAUTH_API_KEY' "http://localhost:9011/api/application/$APPLICATION_ID"
-APPLICATIONJSON="$(curl -s -H "Authorization: $FUSIONAUTH_API_KEY" "http://localhost:9011/api/application/$APPLICATION_ID" )"
-#echo "APPLICATIONJSON=$APPLICATIONJSON"
+APPLICATIONJSON="$(curl -s -H "Authorization: $FUSIONAUTH_API_KEY" "http://localhost:9011/api/application/${APPLICATION_ID}" )"
+echo "APPLICATIONJSON=$APPLICATIONJSON"
 
-CLIENT_ID="$(echo $APPLICATIONJSON|jq -r .application.oauthConfiguration.clientId)"
+
+CLIENT_ID="$(echo ${APPLICATIONJSON}|jq -r .application.oauthConfiguration.clientId)"
 echo "ClientId=$CLIENT_ID"
-CLIENT_SECRET="$(echo $APPLICATIONJSON|jq -r .application.oauthConfiguration.clientSecret)"
+CLIENT_SECRET="$(echo ${APPLICATIONJSON}|jq -r .application.oauthConfiguration.clientSecret)"
 echo "ClientSecret=$CLIENT_SECRET"
 
 
 # ls -latr
 echo " "
-echo "bash ./deviceauth.sh setDiscovery http://localhost:9011/.well-known/openid-configuration"
-bash ./deviceauth.sh setDiscovery http://localhost:9011/.well-known/openid-configuration
+echo "bash ./deviceauth.sh setDiscovery $DISCOVERY_URL"
+bash ./deviceauth.sh setDiscovery $DISCOVERY_URL
 
 echo " "
 echo "bash ./deviceauth.sh setClientid $CLIENT_ID"
