879011f18bafbb4455daba269dbe395d80e53777
jerryhopper <hopper.jerry@gmail.com>
2020-12-03 16:28:00 +0100

    update...

---
+++ tests/tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+deviceauth.sh setDiscovery https://fusionauth:9011/.well-known/openid-configuration
+
+
+deviceauth.sh setClientid 30663132-6464-6665-3032-32646
+
+
+#deviceauth.sh authorize
+
+#deviceauth.sh poll once
+
+#AUTHORIZE
+
+#deviceauth.sh poll once
+
+#deviceauth.sh renew
